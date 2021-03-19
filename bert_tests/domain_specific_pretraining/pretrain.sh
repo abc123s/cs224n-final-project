01136170f6ed7db4f40d3163860966a26be706f6
Peter Zhang <peter0zhang@gmail.com>
2021-03-19 03:41:39 -0700

    remove reference to short corpus for local testing

--- bert_tests/domain_specific_pretraining/pretrain.sh
+++ bert_tests/domain_specific_pretraining/pretrain.sh
@@ -4,6 +4,6 @@ EXPERIMENT_DIR=./experiments/$(date +"%Y%m%d_%H%M")_$(git rev-parse --short HEAD
 
 python run_mlm.py \
     --model_name_or_path bert-base-cased \
-    --train_file ./ing_ins_rec_doc_cased_short.txt \
+    --train_file ./ing_ins_rec_doc_cased.txt \
     --do_train \
     --output_dir $EXPERIMENT_DIR
